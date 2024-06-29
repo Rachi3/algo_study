fd99faf5494c8dfee69c8089751c9c8d5b4f7f38
Rachi3 <rlagusdn715@gmail.com>
2024-06-29 23:19:18 +0900

    주몽 문제 갑옷이 고유한 번호 라는 뜻은 해당 번호를 가진 갑옷은 한개만 있다는 뜻..

--- week1/1940.cpp
+++ week1/1940.cpp
@@ -1,34 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int arr[100010];
-
 int main(){
 	
-	int n,tmp,cnt=0;
-	long long m;
+	int n,m;
 	cin>>n>>m;
 	
-	for(int i=0;i<n;i++)
-	{
-		cin>>tmp;
-		
-		arr[tmp]++;
-	}
+	int arr[n];
 	
-	if(m<=200000){
+	for(int i=0;i<n;i++)cin>>arr[i];
 	
-	for(int i=1;i<m/2;i++){
-		//cout<<i<<" "<<arr[i]<<" "<<m-i<<" "<<arr[m-i]<<"\n";
-		if(arr[i]>0&&arr[m-i]>0){
-			 if(i == m - i) {
-                cnt += arr[i] / 2;
-            } else {
-                cnt += min(arr[i], arr[m - i]);
-            }
+	if(m>200000)
+		cout<<0;
+	else{
+		for(int i=0;i<n;i++){
+			for(int j=i+1;j<n;j++){
+				if(arr[i]+arr[j]==m)
+					cnt++;
+				}
 		}
+		
+		cout<<cnt;
 	}
-}
-	cout<<cnt;
+	
+	
 	return 0;
 }

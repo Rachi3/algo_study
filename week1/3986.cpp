dc37bf00fb49895ed956bd5e11175eebd18368ac
Rachi3 <rlagusdn715@gmail.com>
2024-06-30 00:14:40 +0900

    3986번 모식도 그려서 돌려서도 확인 테트리스 처럼 처리 가능

---
+++ week1/3986.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.tie(NULL);
+	
+	int n,ret=0;
+	string s;
+	
+	cin>>n;
+	
+	for(int i=0;i<n;i++)
+	{
+		stack<char> stk;
+		cin>>s;
+		
+		for(char c:s){
+			
+			if(!stk.empty()&&stk.top()==c)
+				stk.pop();
+			else
+				stk.push(c);
+		}
+		
+		if(stk.empty())
+			ret++;			
+	}
+	
+	cout<<ret;
+	
+	return 0;
+}
